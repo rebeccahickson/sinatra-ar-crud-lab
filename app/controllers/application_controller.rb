07677bb88952ac163289fc4c8a240d8c0f6a50c0
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-03 00:56:06 -0500

    commit tree 7f116d2384cb87432d83b8b6902bd1e649eeac90

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -6,8 +5,53 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/' do
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    a = Article.new
+    a.title = params[:title]
+    a.content = params[:content]
+    a.save
+    redirect "/articles/#{a.id}"
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    title = params[:title]
+    content = params[:content]
+    @article = Article.find_by(id: id)
+    @article.update(title: title, content: content)
+    redirect "/articles/#{id}"
+  end
+
+  delete '/articles/:id' do
+    id = params[:id]
+    Article.destroy(id)
+    redirect "/articles"
   end
 end
